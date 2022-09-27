5d707cd49776946761b94198368d1da4649f197e
Ezequiel Pallini <106202985+ezequielpallini2@users.noreply.github.com>
2022-09-27 13:57:03 -0300

    Add files via upload

---
+++ P2E1.java
@@ -0,0 +1,49 @@
+/*
+ Se dispone de la clase Persona (en la carpeta tema2). Un objeto persona puede crearse
+sin valores iniciales o enviando en el mensaje de creación el nombre, DNI y edad (en ese
+orden). Un objeto persona responde a los siguientes mensajes:
+getNombre() retorna el nombre (String) de la persona
+getDNI() retorna el dni (int) de la persona
+getEdad() retorna la edad (int) de la persona
+setNombre(X) modifica el nombre de la persona al “String” pasado por parámetro (X)
+setDNI(X) modifica el DNI de la persona al “int” pasado por parámetro (X)
+setEdad(X) modifica la edad de la persona al “int” pasado por parámetro (X)
+toString() retorna un String que representa al objeto. Ej: “Mi nombre es Mauro,
+mi DNI es 11203737 y tengo 70 años”
+Realice un programa que cree un objeto persona con datos leídos ddesde teclado. Luego
+muestre en consola la representación de ese objeto en formato String. 
+
+
+
+*/
+package tema2;
+
+/**
+ *
+ * @author usteciow
+ */
+import PaqueteLectura.Lector; 
+
+public class P2E1 {
+    
+    
+    public static void main(String[] args) {
+           Persona per = new Persona();
+           System.out.print("Ingrese DNI: ");
+           per.setDNI(Lector.leerInt());          
+           System.out.print("Ingrese nombre: ");
+           per.setNombre(Lector.leerString());       
+           System.out.print("Ingrese edad: ");
+           per.setEdad(Lector.leerInt());
+           System.out.print(per.toString());
+           
+           
+           
+           
+           
+           
+           
+        
+    }
+    
+}
